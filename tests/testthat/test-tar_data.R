1d9996ab06dc8326799166afcd0b1814babc344e
Ross Wilson <ross.wilson@otago.ac.nz>
2025-11-21 22:12:04 +1300

    add tar_data()

---
+++ tests/testthat/test-tar_data.R
@@ -0,0 +1,64 @@
+targets::tar_test("tar_data() works", {
+  x <- tar_data(mydata, "mydata.csv", read.csv, skip = 1)
+  expect_length(x, 2)
+  expect_equal(x[[1]]$name, "file_mydata")
+  expect_equal(x[[1]]$command$string, "expression(\"raw_data/mydata.csv\")")
+  expect_equal(x[[1]]$settings$description, character(0L))
+  expect_equal(x[[2]]$name, "raw_mydata")
+  expect_equal(
+    x[[2]]$command$string,
+    "expression(read.csv(file_mydata, skip = 1))"
+  )
+  expect_equal(x[[2]]$settings$description, character(0L))
+})
+
+targets::tar_test("tar_data() receives options", {
+  targets::tar_option_set(format = "qs")
+  x <- tar_data(mydata, "mydata.csv", read.csv)
+  expect_equal(x[[1]]$settings$format, "file")
+  expect_equal(x[[2]]$settings$format, "qs")
+})
+
+targets::tar_test("tar_data() sets deployment = 'main' by default", {
+  x <- tar_data(mydata, "mydata.csv", read.csv)
+  expect_equal(x[[1]]$settings$deployment, "main")
+  expect_equal(
+    x[[2]]$settings$deployment,
+    targets::tar_option_get("deployment")
+  )
+})
+
+targets::tar_test("tidy eval works", {
+  envir <- environment()
+  targets::tar_option_set(envir = envir)
+  envir$y <- "mydata.csv"
+  x <- tar_data(mydata, !!y, read.csv)
+  expect_equal(x[[1]]$command$string, "expression(\"raw_data/mydata.csv\")")
+})
+
+targets::tar_test("can disable tidy eval", {
+  y <- "mydata.csv"
+  x <- tar_data(mydata, !!y, read.csv, tidy_eval = FALSE)
+  expect_equal(x[[1]]$command$string, "expression(\"raw_data/!!y\")")
+})
+
+targets::tar_test("no name", {
+  expect_error(
+    tar_data(filename = "mydata.csv", fn = read.csv),
+    class = "tar_condition_validate"
+  )
+})
+
+targets::tar_test("no filename", {
+  expect_error(
+    tar_data(mydata, fn = read.csv),
+    class = "tar_condition_validate"
+  )
+})
+
+targets::tar_test("no fn", {
+  expect_error(
+    tar_data(mydata, "mydata.csv"),
+    class = "tar_condition_validate"
+  )
+})
